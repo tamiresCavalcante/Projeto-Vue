5e244f49a337791eea88cd2c05e20731ebd69952
tamires cavalcante <projetostamirescavalcante@gmail.com>
2021-02-01 07:44:27 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,111 @@
+const vm = new Vue({
+    el: "#app",
+    data: {
+        produtos: [],
+        produto: false,
+        carrinho: [],
+        carrinhoAtivo: false,
+        mensagemAlerta: "Item adicionado",
+        alertaAtivo: false,
+    },
+    filters: {
+        numeroPreco(valor) {
+           return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"});
+        }
+    },
+    computed: {
+        carrinhoTotal() {
+            let total = 0;
+            if(this.carrinho.length) {
+                this.carrinho.forEach(item => {
+                    total += item.preco;
+                })
+            }
+            return total;
+        }
+    },
+    methods: {
+        fetchProdutos() {
+            fetch("https://run.mocky.io/v3/d81863fb-8486-4250-aa4d-19da1d678efe")
+            .then(r => r.json())
+            .then(r => {
+                this.produtos = r;
+            })
+        },
+        fetchProduto(id) {
+           fetch(`./api/produtos/${id}/dados.json`)
+            .then(r => r.json())
+            .then(r => {
+                this.produto = r;
+            })            
+        },
+
+
+        
+        abrirModal(id) {
+            this.fetchProduto(id);
+            window.scrollTo({
+                top:0
+            })
+        },
+
+        fecharModal({target, currentTarget}){
+            if (target === currentTarget)
+                this.produto = false;
+        },
+        clickForaCarrinho({target, currentTarget}) {
+            if(target === currentTarget) this.carrinhoAtivo = false;
+        },
+        adicionarItem(){
+            this.produto.estoque--;
+            const {id, nome, preco} = this.produto;
+            this.carrinho.push({id, nome, preco});
+            this.alerta(`${nome} foi adicionado ao carrinho`);
+        },
+        removerItem(index) {
+            this.carrinho.splice(index,1);
+        },
+        checarLocalStorage() {
+            if(window.localStorage.carrinho) 
+                this.carrinho = JSON.parse(window.localStorage.carrinho);
+        },
+        compararEstoque() {
+            const items = this.carrinho.filter(item => {
+                if(item.id === this.produto.id){
+                    return true;
+                }
+            })
+            this.produto.estoque -= items.length;
+        },
+        alerta(mensagem) {
+            this.mensagemAlerta = mensagem;
+            this.alertaAtivo = true;
+            setTimeout(() => {
+                this.alertaAtivo = false;
+            }, 2000);
+        },
+    },
+    watch: {
+        produto() {
+            document.title = this.produto.nome || "Aditum";
+            if(this.produto) {
+                this.compararEstoque();
+            }
+        },
+        carrinho() {
+            window.localStorage.carrinho = JSON.stringify(this.carrinho);
+        }
+    },
+    created() {
+        this.fetchProdutos();
+        this.checarLocalStorage();
+    }
+})
+
+
+
+
+
+
+
+
